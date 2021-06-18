75c812e14f70098bddae1056db17726dd17a00d5
ylem76 <ylem76@thebreeze.co.kr>
2021-06-18 18:21:31 +0900

    1.2

---
+++ src/routes/EditProfile.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function EditProfile() {
+  return <span>Edit Profile</span>
+}
+export default EditProfile;
---
+++ src/routes/Profile.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Profile() {
+  return <span>Profile</span>
+}
+export default Profile;
