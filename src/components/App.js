2a88fe2230f954b8a3e59f51f74493420cb62e1d
ylem76 <ylem76@thebreeze.co.kr>
2021-06-18 18:26:48 +0900

    2.0.5

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 
+import React, { useState } from 'react';
 import AppRouter from './Router'
+
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return <>
+    <AppRouter isLoggedIn = {isLoggedIn}/>
+    <footer>test</footer>
+  </>;
 }
 
 export default App;
