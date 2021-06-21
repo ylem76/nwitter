f1cbf2515c1206083a65b65fc99d8ae47b9122b4
ylem76 <ylem76@thebreeze.co.kr>
2021-06-21 15:56:39 +0900

    2.6

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Navigation= () => <nav>
+  <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/profile">my profile</Link></li>
+  </ul>
+</nav>
+  
+export default Navigation;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { authService } from '../fBase';
+import {useHistory} from 'react-router-dom';
+
 function Profile() {
-  return <span>Profile</span>
+  const history = useHistory();
+  // 리액트 라우터 이용
+  
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  }
+  return <>
+
+    <button onClick={onLogOutClick}>Log Out</button>
+  </>
 }
 export default Profile;
