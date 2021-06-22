066bde3b3d5a9a9765ac1f83b48a12f10642d0dc
ylem76 <ylem76@thebreeze.co.kr>
2021-06-22 11:50:49 +0900

    3.4

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { dbService } from '../fBase';
+import Nweet from '../components/Nweet'
 
 const Home = ({userObj}) =>{
   const [nweet, setNweet] = useState('');
@@ -55,9 +56,11 @@ const Home = ({userObj}) =>{
       </form>
       <div>
         {nweets.map(nweet => (
-          <div key={nweet.id}>
-          <h4>{nweet.text}</h4>
-        </div>
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
         ))}
       </div>
     </div>
