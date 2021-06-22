c8062b32549357351bb8705fa9839ee257303d27
ylem76 <ylem76@thebreeze.co.kr>
2021-06-22 12:13:55 +0900

    3.5

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { dbService } from '../fBase';
 
-const Nweet = ({nweetObj, isOwner}) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>delete nweet</button>
-        <button>edit nweet</button>
-      </>
-    )}
-  </div>
-);
+const Nweet = ({nweetObj, isOwner}) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text)
+  const onDeleteClick = async () => {
+    const ok = window.confirm('삭제하겠습니다. ok?');
+    if(ok){
+      //delete
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    } else {
+      // not delete
+
+    }
+    console.log(ok);
+
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    console.log(nweetObj, newNweet)
+    event.preventDefault();
+    await dbService.doc(`nweets/${nweetObj.id}`).update({
+      text:newNweet
+    })
+    setEditing(false);
+  }
+  const onChange = (event) => {
+    const {target: {value}} = event;
+    setNewNweet(value);
+  }
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input
+              type="submit"
+              value="Update Nweet"
+            />
+          </form>
+          <button>edit</button>
+          <button onClick={toggleEditing}>cancel</button>
+        </>
+      ) : (
+        <>
+          <div>
+              <h4>{nweetObj.text}</h4>
+              {isOwner && (
+                <>
+                  <button onClick={onDeleteClick}>delete nweet</button>
+                  <button onClick={toggleEditing}>edit nweet</button>
+                </>
+              )}
+          </div>
+        </>
+      )}
+    </div>
+  )
+};
 
 
 export default Nweet;
